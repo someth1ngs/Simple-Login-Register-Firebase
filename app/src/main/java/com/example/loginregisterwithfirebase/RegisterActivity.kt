be836717ff846ea8e6683f19d7f8ce358962376b
Dewangga <hamadakinose@gmail.com>
2023-12-07 14:03:35 +0700

    Login/Register with Firebase

---
+++ app/src/main/java/com/example/loginregisterwithfirebase/RegisterActivity.kt
@@ -0,0 +1,65 @@
+package com.example.loginregisterwithfirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.loginregisterwithfirebase.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityRegisterBinding
+    lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvToLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener{
+            val email = binding.edtEmailRegister.text.toString()
+            val password = binding.edtPasswordRegister.text.toString()
+
+            if(email.isEmpty()){
+                binding.edtEmailRegister.error = "Email kosong!!!"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.edtEmailRegister.error = "Email kosong!!!"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            if(password.isEmpty()){
+                binding.edtPasswordRegister.error = "Password kosong!!!"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            RegisterFirebase(email, password)
+        }
+    }
+
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Register Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
